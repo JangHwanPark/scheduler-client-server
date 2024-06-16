5ee4121a663156bfc8bde317ba38c0462c5f1518
namung08 <namung08@gmail.com>
2024-06-16 19:20:13 +0900

    feat: spring default setting

---
+++ GameEquipmentRental/src/main/java/com/game/rental/GameEquipmentRentalApplication.java
@@ -0,0 +1,13 @@
+package com.game.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameEquipmentRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameEquipmentRentalApplication.class, args);
+	}
+
+}
