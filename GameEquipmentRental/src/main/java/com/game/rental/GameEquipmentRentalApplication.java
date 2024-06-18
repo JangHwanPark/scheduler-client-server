b79eee34b83a8b82b1fbf09b5663dde793bcc842
japark <wkd5878@gmail.com>
2024-06-18 17:02:26 +0900

    feat: 프론트엔드 프록시 설정

--- GameEquipmentRental/src/main/java/com/game/rental/GameEquipmentRentalApplication.java
+++ GameEquipmentRental/src/main/java/com/game/rental/GameEquipmentRentalApplication.java
@@ -5,13 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Date;
+
 @SpringBootApplication
+@RestController
 public class GameEquipmentRentalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GameEquipmentRentalApplication.class, args);
 	}
-
 
 	// CORS Setting
 	// http://localhost:5173 모든 요청 허용
