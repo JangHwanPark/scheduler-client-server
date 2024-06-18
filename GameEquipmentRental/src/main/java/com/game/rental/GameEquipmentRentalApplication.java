e1eee8c15a0f1e1ab6c4a9d953fc92cb88f61230
japark <wkd5878@gmail.com>
2024-06-18 16:55:28 +0900

    frontend init

--- GameEquipmentRental/src/main/java/com/game/rental/GameEquipmentRentalApplication.java
+++ GameEquipmentRental/src/main/java/com/game/rental/GameEquipmentRentalApplication.java
@@ -2,6 +2,8 @@ package com.game.rental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GameEquipmentRentalApplication {
@@ -10,4 +12,19 @@ public class GameEquipmentRentalApplication {
 		SpringApplication.run(GameEquipmentRentalApplication.class, args);
 	}
 
+
+	// CORS Setting
+	// http://localhost:5173 모든 요청 허용
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+				// 모든 경로, http 메소드, http://localhost:5173 도메인 허용
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:5173");
+			}
+		};
+	}
 }
