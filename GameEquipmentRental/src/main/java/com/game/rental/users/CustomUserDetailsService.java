30e4b2392afc1fe286bd5cc001d258c73972a6b8
namung08 <namung08@gmail.com>
2024-06-19 17:04:31 +0900

    feat: 로그인 처리를 위한 사용자의 정보를 확인 하는 서비스 입니다.

---
+++ GameEquipmentRental/src/main/java/com/game/rental/users/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.game.rental.users;
+
+import com.game.rental.users.entity.UserEntity;
+import com.game.rental.users.entity.UserEntityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserEntityRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByLoginId(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return User.builder()
+                .username(user.getLoginId())
+                .password(user.getLoginPassword())
+                .roles(user.getRoles())
+                .build();
+    }
+}
