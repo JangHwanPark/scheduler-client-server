59330694ccbe3141a8e39115c7fc15aaf92545b0
user <wkd5878@gmail.com>
2024-06-21 09:39:59 +0900

    design: MainHeader.tsx 스타일 적용 (임시)

--- GameEquipmentRental/src/main/frontend/src/components/layout/MainHeader.tsx
+++ GameEquipmentRental/src/main/frontend/src/components/layout/MainHeader.tsx
@@ -1,22 +1,24 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function MainHeader() {
     return (
-        <header>
-            <h1>ATTACK</h1>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to={"/login"}>로그인</Link>
-                    </li>
-                    <li>
-                        <Link to={"/register"}>회원가입</Link>
-                    </li>
-                    <li>
-                        <Link to={"/admin"}>어드민</Link>
-                    </li>
-                </ul>
-            </nav>
+        <header className="main-header">
+            <div className="main-header-wrapper">
+                <h1 className="main-header-title">ATTACK</h1>
+                <nav className="main-header-nav">
+                    <ul className="main-header-nav-list">
+                        <li className="main-header-nav-list-item">
+                            <Link to="/register" className="main-header-nav-list-item-link">회원가입</Link>
+                        </li>
+                        <li className="main-header-nav-list-item">
+                            <Link to="/admin" className="main-header-nav-list-item-link">어드민</Link>
+                        </li>
+                        <li className="main-header-nav-list-item">
+                            <Link to="/login" className="main-header-nav-list-item-link">로그아웃</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 }
