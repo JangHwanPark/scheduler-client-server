4964c887a7270d4c2a30a355486ef89b345e7547
namung08 <namung08@gmail.com>
2024-06-23 19:11:30 +0900

    feat: JWTUtil 추가

--- GameEquipmentRental/src/main/java/com/game/rental/security/config/SecurityConfig.java
+++ GameEquipmentRental/src/main/java/com/game/rental/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.game.rental.security.config;
 
 
 import com.game.rental.security.jwt.filter.LoginFilter;
+import com.game.rental.security.jwt.util.JWTUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +21,7 @@ public class SecurityConfig {
 
     //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
     private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
 
     //AuthenticationManager Bean 등록
     @Bean
@@ -49,7 +51,7 @@ public class SecurityConfig {
 
         //필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
         http
-                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration),jwtUtil), UsernamePasswordAuthenticationFilter.class);
         // jwt 를 이용하여 로그인 로직을 구현시 session 을 사용하지 않아 session을 stateless 상태로 설정
         http
                 .sessionManagement((session) -> session
