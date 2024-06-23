10ec0f869960230c5937948cb5c4f92820be00de
namung08 <namung08@gmail.com>
2024-06-23 22:53:27 +0900

    feat: 페이지의 접근한 사용자의 id 와 권한을 페이지에 출력해주는 부분 추가

--- GameEquipmentRental/src/main/java/com/game/rental/security/SecurityTestController.java
+++ GameEquipmentRental/src/main/java/com/game/rental/security/SecurityTestController.java
@@ -1,18 +1,40 @@
 package com.game.rental.security;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @RestController
 public class SecurityTestController {
     @GetMapping("/api1")
     public ResponseEntity<?> api1() {
-        return ResponseEntity.ok("api1");
+        // 접속한 사용자 이름 id 값
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        // 사용자 role 값
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+        return ResponseEntity.ok("api1 name : "+name+" role : "+role);
     }
 
     @GetMapping("/api2")
     public ResponseEntity<?> api2() {
-        return ResponseEntity.ok("api2");
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+        return ResponseEntity.ok("api2 name : "+name +" role : "+role);
     }
 }
