466bff13af1136c2fe4e5fcfd4ed04b3ff7641ae
namung08 <namung08@gmail.com>
2024-06-24 17:44:06 +0900

    feat: refresh 토큰을 통해 access 토큰을 재발급 하는 기능 추가

--- GameEquipmentRental/src/main/java/com/game/rental/security/ReissueController.java
+++ GameEquipmentRental/src/main/java/com/game/rental/security/ReissueController.java
@@ -17,38 +17,51 @@ public class ReissueController {
     private final JWTUtil jwtUtil;
 
     @PostMapping("/reissue")
-    public ResponseEntity<?> reissue(HttpServletRequest req, HttpServletResponse res) {
-        // get refresh token
+    public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
+        //get refresh token
         String refresh = null;
-        Cookie[] cookies = req.getCookies();
+        Cookie[] cookies = request.getCookies();
         for (Cookie cookie : cookies) {
+
             if (cookie.getName().equals("refresh")) {
+
                 refresh = cookie.getValue();
             }
         }
-        // if refresh token is null redirect bad_request
+
         if (refresh == null) {
+
+            //response status code
             return new ResponseEntity<>("refresh token null", HttpStatus.BAD_REQUEST);
         }
-        // refresh token expired check
+
+        //expired check
         try {
             jwtUtil.isExpired(refresh);
         } catch (ExpiredJwtException e) {
-            // response status code
+
+            //response status code
             return new ResponseEntity<>("refresh token expired", HttpStatus.BAD_REQUEST);
         }
+
+        // 토큰이 refresh인지 확인 (발급시 페이로드에 명시)
         String category = jwtUtil.getCategory(refresh);
-        // refresh token check category is refresh
+
         if (!category.equals("refresh")) {
-            // response status code
-            return new ResponseEntity<>("refresh token invalid", HttpStatus.BAD_REQUEST);
+
+            //response status code
+            return new ResponseEntity<>("invalid refresh token", HttpStatus.BAD_REQUEST);
         }
-        String id = jwtUtil.getUsername(refresh);
+
+        String username = jwtUtil.getUsername(refresh);
         String role = jwtUtil.getRole(refresh);
 
-        // make new access token
-        String newAccess = jwtUtil.createJwt(category,id,role,600000L);
-        res.setHeader("access", newAccess);
-        return new ResponseEntity<>("success", HttpStatus.OK);
+        //make new JWT
+        String newAccess = jwtUtil.createJwt("access", username, role, 600000L);
+
+        //response
+        response.setHeader("access", newAccess);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
