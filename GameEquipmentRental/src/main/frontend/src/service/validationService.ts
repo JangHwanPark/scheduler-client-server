6b0ff8ddb4732a8de972c847bff4ba085eafa0ac
user <wkd5878@gmail.com>
2024-06-27 07:38:39 +0900

    refactoring: Validation 비밀번호 형식 검증로직 특수문자도 포함하도록 수정

--- GameEquipmentRental/src/main/frontend/src/service/validationService.ts
+++ GameEquipmentRental/src/main/frontend/src/service/validationService.ts
@@ -1,7 +1,7 @@
 import {UserInput} from "../types";
 
 // 이메일 형식 검사 (정규식 사용)
-export const validateEmail = (email: string): boolean => {
+export const validateEmail = (email: string | undefined): boolean => {
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return re.test(String(email).toLowerCase());
 };
@@ -11,9 +11,9 @@ export const validatePassword = (password: string): boolean => {
     return password.length >= 8;
 };
 
-// 비밀번호 형식 검사
+// 비밀번호 형식 검사 (하나 이상의 대문자, 소문자, 숫자, 특수문자가 포함되어야 함)
 export const validatePasswordFormat = (password: string): boolean => {
-    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,}$/;
     return re.test(password);
 }
 
@@ -24,6 +24,7 @@ export const validateTrim = (input: string | undefined): boolean => {
 }
 
 
+// 사용자 입력 검증 함수
 export const validateUserInput = (userInput: UserInput): boolean => {
     if (!validateTrim(userInput.username)) {
         alert("아이디를 입력해 주세요.");
@@ -35,15 +36,15 @@ export const validateUserInput = (userInput: UserInput): boolean => {
         return false;
     }
 
-    /*if (!validateEmail(userInput.id)) {
+    if (!validateEmail(userInput.username)) {
         alert("이메일 형식이 올바르지 않습니다.");
         return false;
-    }*/
+    }
 
-    /*if (!validatePassword(userInput.password)) {
+    if (!validatePassword(userInput.password)) {
         alert("비밀번호는 8자 이상이어야 합니다.");
         return false;
-    }*/
+    }
 
     if (userInput.password !== userInput.passwordConfirm) {
         alert("비밀번호가 일치하지 않습니다.");
